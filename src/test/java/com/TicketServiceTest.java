7ef3d805b1efc05972784fc29f6d3068232c80fa
Gaur <mukul.gaur@capgemini.com>
2022-01-24 12:01:52 +0530

    ticket test cases

--- src/test/java/com/TicketServiceTest.java
+++ src/test/java/com/TicketServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.entity.Activity;
+import com.entity.Customer;
 import com.entity.Ticket;
 import com.repository.TicketRepository;
 import com.service.TicketService;
@@ -28,17 +31,137 @@ class TicketServiceTest {
 	@Test
 	void testInsertTicket() {
 		
+		Activity a = new Activity();
+		a.setActivityId(1);
+		a.setActivityName("Gaint Wheel");
+		a.setCharges(150);
+		a.setDescription("Land game");
+		
+		Customer c1=new Customer();
+		c1.setUserId(1);
+		c1.setEmail("customer@gmail.com");
+		c1.setFirstName("Geetha");
+		c1.setLastName("Madhuri");
+		c1.setMobileNumber("9988445512");
+		c1.setPassword("Geeth@12");
+		c1.setUsername("Geetmadhu");
+		
+		Ticket t = new Ticket();
+		t.setTicketId(1);
+		t.setBill(500);
+		t.setActivity(a);
+		t.setCustomer(c1);
+		
+		
+		Mockito.when(ticketRepo.save(t)).thenReturn(t);
+		assertThat(ticketService.insertTicket(t)).isEqualTo(t);
+		
+	}
+
+	@Test
+	void testUpdateTicketById() throws Throwable {
+		Activity a1 = new Activity();
+		a1.setActivityId(1);
+		a1.setActivityName("Gaint Wheel");
+		a1.setCharges(150);
+		a1.setDescription("Land game");
+		
+		Customer c1=new Customer();
+		c1.setUserId(1);
+		c1.setEmail("customer@gmail.com");
+		c1.setFirstName("Geetha");
+		c1.setLastName("Madhuri");
+		c1.setMobileNumber("9988445512");
+		c1.setPassword("Geeth@12");
+		c1.setUsername("Geetmadhu");
+		
+		Ticket t = new Ticket();
+		t.setTicketId(1);
+		t.setBill(500);
+		t.setActivity(a1);
+		t.setCustomer(c1);
+		
+		Optional<Ticket> t1 = Optional.of(t);
+		Mockito.when(ticketRepo.findById(1)).thenReturn(t1);
+		
+		Mockito.when(ticketRepo.save(t)).thenReturn(t);
+		
+		assertThat(ticketService.updateTicketById(t, 1)).isEqualTo(t);
+	}
+
+	@Test
+	void testDeleteTicketById() {
+		Activity a1 = new Activity();
+		a1.setActivityId(1);
+		a1.setActivityName("Gaint Wheel");
+		a1.setCharges(150);
+		a1.setDescription("Land game");
+		
+		Customer c1=new Customer();
+		c1.setUserId(1);
+		c1.setEmail("customer@gmail.com");
+		c1.setFirstName("Geetha");
+		c1.setLastName("Madhuri");
+		c1.setMobileNumber("9988445512");
+		c1.setPassword("Geeth@12");
+		c1.setUsername("Geetmadhu");
+		
+		Ticket t = new Ticket();
+		t.setTicketId(1);
+		t.setBill(500);
+		t.setActivity(a1);
+		t.setCustomer(c1);
+		
+		Optional<Ticket> t1 = Optional.of(t);
+		
+		Mockito.when(ticketRepo.findById(1)).thenReturn(t1);
+		Mockito.when(ticketRepo.existsById(t.getTicketId())).thenReturn(false);
+		assertFalse(ticketRepo.existsById(t.getTicketId()));
+	}
+
+	@Test
+	void testGetAllTickets() {
+		Activity a1 = new Activity();
+		a1.setActivityId(1);
+		a1.setActivityName("Gaint Wheel");
+		a1.setCharges(150);
+		a1.setDescription("Land game");
+		
+		Activity a2 = new Activity();
+		a2.setActivityId(2);
+		a2.setActivityName("Wave pool");
+		a2.setCharges(150);
+		a2.setDescription("Water game");
+		
+		Customer c1=new Customer();
+		c1.setUserId(1);
+		c1.setEmail("customer@gmail.com");
+		c1.setFirstName("Geetha");
+		c1.setLastName("Madhuri");
+		c1.setMobileNumber("9988445512");
+		c1.setPassword("Geeth@12");
+		c1.setUsername("Geetmadhu");
+		
+		Customer c2=new Customer();
+		c2.setUserId(1);
+		c2.setEmail("customer@gmail.com");
+		c2.setFirstName("user");
+		c2.setLastName("test");
+		c2.setMobileNumber("9988445512");
+		c2.setPassword("user@12");
+		c2.setUsername("test");
+		
 		Ticket t1 = new Ticket();
 		t1.setTicketId(1);
-		t1.setBill(120);
-//		t1.setDateTime("2022-01-09 12:14:53.287000");
-//		t1.setDescription("Land game");
+		t1.setBill(500);
+		t1.setActivity(a1);
+		t1.setCustomer(c1);
 		
 		Ticket t2 = new Ticket();
 		t2.setTicketId(1);
-		t2.setBill(120);
-//		t2.setDateTime("2022-01-09 12:14:53.287000");
-//		t2.setDescription("Land game");
+		t2.setBill(500);
+		t2.setActivity(a2);
+		t2.setCustomer(c2);
 		
 		List<Ticket> ticketList = new ArrayList<>();
 		ticketList.add(t1);
@@ -47,26 +170,6 @@ class TicketServiceTest {
 		Mockito.when(ticketRepo.findAll()).thenReturn(ticketList);
 		assertThat(ticketService.getAllTickets()).isEqualTo(ticketList);
 		
-	}
-
-	@Test
-	void testUpdateTicketById() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testDeleteTicketById() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetAllTickets() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetTicketById() {
-		fail("Not yet implemented");
 	}
 
 }
